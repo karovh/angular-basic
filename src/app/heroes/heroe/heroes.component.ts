b9868cbf9e066cbf4c82bab2056c32942ab16c3e
Carolinavh <carolinavillamilh@gmail.com>
2022-09-02 18:01:59 -0500

    primer Commit

---
+++ src/app/heroes/heroe/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+
+})
+export class HeroeComponent {
+    nombre: string = 'ironman';
+    edad: number = 45
+
+    get nombreCapitalizado() {
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return ` el nombre es ${this.nombre} y la edad es ${this.edad}`
+
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'spiderman';
+
+    }
+
+    cambiarEdad(): void {
+        this.edad = 30;
+    }
+
+
+
+}
